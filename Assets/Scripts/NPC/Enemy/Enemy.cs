7c3746f0494c79a424175cad71ae1279856bda07
Usek777 <91222585+Usek777@users.noreply.github.com>
2023-05-07 12:28:24 +0300

    corpse fix

--- Assets/Scripts/NPC/Enemy/Enemy.cs
+++ Assets/Scripts/NPC/Enemy/Enemy.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
@@ -16,11 +17,80 @@ public class Enemy : MonoBehaviour
     public Sprite DeadEnemySprite;
     public bool IsDead = false;
 
+
+
+
+
+
+
+
+
+
+    private void ActivateVisibility() => GetComponent<Visibility>().enabled = true;
+
+
+
+
+
+
+
+
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+        GetComponent<Visibility>().enabled = false;
+
+        // ¬ключаю агр врагу через 200мс, чтобы между переходами на другие кровни он не стрел€л по игроку
+        Invoke("ActivateVisibility", 0.2f);
+    }
+
+
+
+
+
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "MainMenu") { Destroy(transform.gameObject); }
+    }
+
+
+
+
+
+
+
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+
+
+
+
+
+
+
+
     public void TakeDamage(int damage)
     {
         health -= damage;
         if (health <= 0) { Die(); }
     }
+
+
+
+
+
+
+
+
+
+
 
     void Die()
     {
@@ -29,5 +99,8 @@ public class Enemy : MonoBehaviour
         IsDead = true;
         GetComponent<CircleCollider2D>().enabled = false;
         this.transform.Rotate(0, 0, 60f);
+
+        // ƒелаю объект не уничтожаемым при загрузке, чтобы трупы не исчезали при переходе
+        DontDestroyOnLoad(transform.parent.gameObject);
     }
 }
